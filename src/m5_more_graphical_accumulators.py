48f55a88f5790f9b55b12e916f98154c3cc6194b
fusarocr <fusarocr@rose-hulman.edu>
2017-12-03 23:29:20 -0500

    circle inscribed

--- src/m5_more_graphical_accumulators.py
+++ src/m5_more_graphical_accumulators.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python. 
   
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Christopher Fusaro.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -98,7 +98,7 @@ def draw_squares_from_circle(n, circle, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -111,6 +111,23 @@ def draw_squares_from_circle(n, circle, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    x = circle.center.x
+    y = circle.center.y
+    radius = circle.radius
+
+    for k in range(n):
+
+        square = rg.Square(rg.Point(x,y), 2 * radius)
+
+        square.attach_to(window)
+
+        x = x + radius
+        y = y + radius
+
+    circle.attach_to(window)
+
+    window.render()
 
 
 def run_test_draw_circles_from_rectangle():
